3d8eeb424630046e6a4d00e9065ff13f55894cbf
Dokgo <nskhelifi@gmail.com>
2023-07-14 10:39:35 +0200

    all css files

--- js/templates/PhotographerCard.js
+++ js/templates/PhotographerCard.js
@@ -7,17 +7,19 @@ class PhotographerCard {
         const $wrapper = document.createElement('article');
         $wrapper.classList.add('photographer-card')
 
+        //photographer.html/id=${this._photographer.id} --> quel lien mettre pour accéder à la page du photographe ??
+
         const photographerCard = `
-            <a href="#">
-                <img class="photographerCard__pp" 
+            <a href="#"> 
+                <img class="photographer-card__pp" 
                     src="${this._photographer.portrait}" 
                     alt="">
                 <h2>${this._photographer.name}</h2>
             </a>
 
-            <p class="photographerCard__details photographerCard__details--localisation">${this._photographer.localisation}</p>
-            <p class="photographerCard__details photographerCard__details--tagline">${this._photographer.tagline}</p>
-            <p class="photographerCard__details photographerCard__details--price">${this._photographer.price}</p>
+            <p class="photographerCard__details photographer-card__details--localisation">${this._photographer.localisation}</p>
+            <p class="photographerCard__details photographer-card__details--tagline">${this._photographer.tagline}</p>
+            <p class="photographerCard__details photographer-card__details--price">${this._photographer.price}</p>
         `
 
         $wrapper.innerHTML = photographerCard;
